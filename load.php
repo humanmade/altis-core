778355aeaec488b02dd173015f71dc3c0fe13332
Joe Hoyle <joehoyle@gmail.com>
2019-03-06 16:54:56 +0530

    Fix NS call

--- load.php
+++ load.php
@@ -4,4 +4,4 @@
  * Main entry point loader for the Core module.
  */
 
-add_filter( 'plugins_url', __NAMESPACE__ . '\\fix_plugins_url', 10, 3 );
+add_filter( 'plugins_url', 'HM\\Platform\\fix_plugins_url', 10, 3 );
