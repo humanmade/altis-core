67a8489f1cb1b6414bad88c8fd2b16f1e44aaa62
Joe Hoyle <joehoyle@gmail.com>
2019-05-16 14:26:39 +0200

    Register composer installer on init
    Composer installers take precedence by latest-added. Out installer can get added before the composer-installers installer. That means that it will be responsible for installing wordpress-plugin packages, which means we don't get to install them into `vendor` based off our whitelist.
    
    We instead register the installer "late" on `init`, which happens after all plugins are loaded, but before packages are installed.

--- inc/composer/class-plugin.php
+++ inc/composer/class-plugin.php
@@ -3,10 +3,11 @@
 namespace Altis\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class Plugin implements PluginInterface {
+class Plugin implements PluginInterface, EventSubscriberInterface {
 
 	/**
 	 * Called when the plugin is activated.
@@ -15,14 +16,30 @@ class Plugin implements PluginInterface {
 	 * @param IOInterface $io
 	 */
 	public function activate( Composer $composer, IOInterface $io ) {
-		$default_installer = $composer->getInstallationManager()->getInstaller( 'library' );
-		$current_installer = $composer->getInstallationManager()->getInstaller( 'wordpress-plugin' );
-		if ( $current_installer && $current_installer !== $default_installer ) {
-			$composer->getInstallationManager()->removeInstaller( $current_installer );
-		}
+		$this->composer = $composer;
+		$this->io = $io;
 
-		$installer = new Installer( $io, $composer );
-		$composer->getInstallationManager()->addInstaller( $installer );
+	}
 
+	/**
+	 * Get the events the plugin subscribed to.
+	 *
+	 * @return array
+	 */
+	public static function getSubscribedEvents() {
+		return [
+			'init' => 'init',
+		];
+	}
+
+	/**
+	 * Register the installer on the `init` hook.
+	 *
+	 * We want to register later than the composer-installers installer
+	 * as the last-added installer takes precedence.
+	 */
+	public function init() {
+		$installer = new Installer( $this->io, $this->composer );
+		$this->composer->getInstallationManager()->addInstaller( $installer );
 	}
 }
