5ab9640924bcdc782b23154391ed50ee2ecc3a0c
Chris Reynolds <chris@humanmade.com>
2020-09-15 14:03:26 -0600

    remove our fork and re-add the original consent api package

--- inc/composer/class-installer.php
+++ inc/composer/class-installer.php
@@ -43,7 +43,6 @@ class Installer extends BaseInstaller {
 			'altis/aws-analytics',
 			'altis/browser-security',
 			'altis/consent',
-			'altis/consent-api',
 			'altis/experiments',
 			'darylldoyle/safe-svg',
 			'humanmade/amp',
@@ -73,6 +72,7 @@ class Installer extends BaseInstaller {
 			'humanmade/wp-seo',
 			'humanmade/wp-simple-saml',
 			'johnbillion/query-monitor',
+			'rlankhorst/wp-consent-level-api',
 			'stuttter/ludicrousdb',
 			'stuttter/wp-user-signups',
 		];
