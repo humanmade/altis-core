9ee770a3b9db37151f8b369fdae5eeb004010ccb
Chris Reynolds <chris@jazzsequence.com>
2020-11-25 14:14:45 -0700

    remove unused callback functions

--- inc/consent/namespace.php
+++ inc/consent/namespace.php
@@ -25,35 +25,3 @@ function load_plugins() {
 		require_once Altis\ROOT_DIR . '/vendor/altis/consent/plugin.php';
 	}
 }
-
-/**
- * Set the analytics script to no-op if no consent to statistics has been granted.
- *
- * @param bool $noop No-op value to prevent analytics events from being sent to Pinpoint. Defaults to false.
- *
- * @return bool      The updated noop value.
- */
-function set_analytics_noop( bool $noop ) : bool {
-	// Override no-op if we don't have consent for statistics.
-	if ( ! wp_has_consent( 'statistics' ) ) {
-		return true;
-	}
-
-	return $noop;
-}
-
-/**
- * Update the Endpoint/User data if we don't have consent for statistics.
- * Overrides with an empty array if we don't have consent.
- *
- * @param array $data An array of analytics data to send to Pinpoint.
- *
- * @return array      The filtered analytics variable data.
- */
-function set_analytics_data( array $data ) : array {
-	if ( ! wp_has_consent( 'statistics' ) ) {
-		$data['Endpoint']->User = [];
-	}
-
-	return $data;
-}
