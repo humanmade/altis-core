253d17e41e03f5a843705f604b967f0ebcdb2b3d
Chris Reynolds <chris@humanmade.com>
2020-11-30 15:56:16 +0000

    adds altis/consent-api to installer
    Co-authored-by: Robert O'Rourke <robert@o-rourke.org>

--- inc/composer/class-installer.php
+++ inc/composer/class-installer.php
@@ -43,6 +43,7 @@ class Installer extends BaseInstaller {
 			'altis/aws-analytics',
 			'altis/browser-security',
 			'altis/consent',
+			'altis/consent-api',
 			'altis/experiments',
 			'darylldoyle/safe-svg',
 			'humanmade/aws-rekognition',
