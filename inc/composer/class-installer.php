bd5f6b3388cbeb10ae8b1f609655bb97c067f884
Robert O'Rourke <robert@o-rourke.org>
2020-12-04 13:24:31 +0000

    Add HM fork of ludicrousdb to installer safelist

--- inc/composer/class-installer.php
+++ inc/composer/class-installer.php
@@ -39,6 +39,7 @@ class Installer extends BaseInstaller {
 			'humanmade/hm-gtm',
 			'humanmade/hm-redirects',
 			'humanmade/hm-limit-login-attempts',
+			'humanmade/ludicrousdb',
 			'humanmade/meta-tags',
 			'humanmade/msm-sitemap',
 			'humanmade/publication-checklist',
