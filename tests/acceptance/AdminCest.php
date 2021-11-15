25b1cde5c41929849b040dd1eb244af494ce623a
Shady Sharaf <shady@sharaf.me>
2021-11-15 17:20:40 +0200

    [qa] Check proper module versions in about page test

--- tests/acceptance/AdminCest.php
+++ tests/acceptance/AdminCest.php
@@ -5,6 +5,8 @@
  * phpcs:disable WordPress.Files, WordPress.NamingConventions, PSR1.Classes.ClassDeclaration.MissingNamespace, HM.Functions.NamespacedFunctions
  */
 
+use Codeception\Util\Locator;
+
 /**
  * Test core module admin features.
  */
@@ -18,30 +20,45 @@ class AdminCest {
 	 * @return void
 	 */
 	public function moduleVersionsDisplayed( AcceptanceTester $I ) {
-		$I->wantToTest( 'About page shows module versions.' );
+		$I->wantToTest( 'About page shows correct module versions.' );
 		$I->loginAsAdmin();
 		$I->amOnAdminPage( 'about.php' );
 		$I->see( 'Current Module Versions' );
+
+		$composer = json_decode( file_get_contents( 'vendor/composer/installed.json' ) );
+		$packages = $composer->packages;
+
 		$modules = [
-			'CMS',
-			'Analytics',
-			'Cloud',
-			'Core',
-			'Developer Tools',
-			'Documentation',
-			'Search',
-			'Local Chassis',
-			'Local Server',
-			'Media',
-			'Multilingual',
-			'Privacy',
-			'Security',
-			'SEO',
-			'SSO',
-			'Workflow',
+			'altis/cms' => [ 'name' => 'CMS' ],
+			'altis/analytics' => [ 'name' => 'Analytics' ],
+			'altis/cloud' => [ 'name' => 'Cloud' ],
+			'altis/core' => [ 'name' => 'Core' ],
+			'altis/dev-tools' => [ 'name' => 'Developer Tools' ],
+			'altis/documentation' => [ 'name' => 'Documentation' ],
+			'altis/enhanced-search' => [ 'name' => 'Search' ],
+			'altis/local-chassis' => [ 'name' => 'Local Chassis' ],
+			'altis/local-server' => [ 'name' => 'Local Server' ],
+			'altis/media' => [ 'name' => 'Media' ],
+			'altis/multilingual' => [ 'name' => 'Multilingual' ],
+			'altis/privacy' => [ 'name' => 'Privacy' ],
+			'altis/security' => [ 'name' => 'Security' ],
+			'altis/seo' => [ 'name' => 'SEO' ],
+			'altis/sso' => [ 'name' => 'SSO' ],
+			'altis/workflow' => [ 'name' => 'Workflow' ],
 		];
+
+		foreach ( $packages as $package ) {
+			if ( false === strpos( $package->name, 'altis/' ) || ! in_array( $package->name, array_keys( $modules ) ) ) {
+				continue;
+			}
+			$modules[ $package->name ]['version'] = $package->version;
+			$modules[ $package->name ]['hash'] = $package->dist->reference;
+		}
+
 		foreach ( $modules as $module ) {
-			$I->see( $module );
+			$tr = Locator::contains( 'tr', $module['name'] );
+			$I->see( $module['version'], $tr );
+			$I->see( $module['hash'], $tr );
 		}
 	}
 
