eebc266bc5b45e48008010b72bd494b01b4e940a
Robin Devitt <robin@humanmade.com>
2022-07-11 14:50:39 +0000

    Update Segment tracking with the page title.

--- inc/telemetry/namespace.php
+++ inc/telemetry/namespace.php
@@ -299,12 +299,23 @@ function get_environment_details() : array {
  * @return void
  */
 function load_segment_js() {
+	$page_name = get_admin_page_title();
+	switch ( $page_name ) {
+		case 'Home':
+			$page_name = 'Dashboard';
+			break;
+		case 'Welcome':
+			$page_name = 'Developer Documentation';
+			break;
+		default:
+			$page_name;
+	}
 	?>
 	<script>
 		// Segment - Load
 		!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="<?php echo esc_js( get_segment_id() ); ?>";analytics.SNIPPET_VERSION="4.13.2";
 			analytics.load( <?php echo wp_json_encode( get_segment_id() ) ?> );
-			analytics.page();
+			analytics.page( "<?php echo $page_name ;?>" );
 		}}();
 	</script>
 	<?php
