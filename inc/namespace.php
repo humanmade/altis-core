586213dd480eda2e9750d3a0d56e2a4ab398e6d4
Ryan McCue <me@ryanmccue.info>
2024-05-27 16:58:29 +0100

    Add API to get secrets/variables

--- inc/namespace.php
+++ inc/namespace.php
@@ -585,3 +585,23 @@ function powered_by( $format = null ) {
 		)
 	);
 }
+
+/**
+ * Get the value of a variable or secret.
+ *
+ * This includes both secrets and non-secret variables.
+ *
+ * @param string $name Name of the variable or secret. Must match the name in the Altis Dashboard exactly.
+ * @param mixed $default Default value to return if not set (such as in local environments).
+ * @return string|null Value as a string if set, null otherwise.
+ */
+function get_variable( string $name ) : ?string {
+	if ( strlen( $name ) > 100 ) {
+		// phpcs:ignore HM.Security.EscapeOutput.OutputNotEscaped
+		trigger_error( 'Secret names must be less than 100 characters.', E_USER_WARNING );
+		return null;
+	}
+
+	$value = getenv( 'USER_' . $name );
+	return $value === false ? null : $value;
+}
